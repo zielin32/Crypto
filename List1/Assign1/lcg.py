8d75d6524c227a0f7cbf764ef99016d4129545c2
zielin32 <zielin32@gmail.com>
2018-03-03 16:11:16 +0100

    Improved the extended gcd function, works for much more cases now! (still needs some refactoring though)

--- List1/Assign1/lcg.py
+++ List1/Assign1/lcg.py
@@ -13,19 +13,34 @@ class LinearCongruentialGenerator(object):
         self.state = (self.state * self.multiplier + self.increment) % self.modulus
         return self.state
 
-def egcd(a, b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        g, x, y = egcd(b % a, a)
-        return (g, y - (b // a) * x, x)
+# def egcd(a, modulus):
+#     if a == 0:
+#         return (modulus, 0, 1)
+#     else:
+#         g, x, y = egcd(modulus % a, a)
+#         return (g, y - (modulus // a) * x, x)
 
-def calculate_modular_inverse(b, n):
-    g, x, _ = egcd(b, n)
-    if g == 1:
-        return x % n
-    else:
-        raise ValueError
+# def calculate_modular_inverse(b, modulus):
+#     g, x, _ = egcd(b, modulus)
+#     if g == 1:
+#         return x % modulus
+#     else:
+#         raise ValueError
+
+def extended_gcd(aa, bb):
+    lastremainder, remainder = abs(aa), abs(bb)
+    x, lastx, y, lasty = 0, 1, 1, 0
+    while remainder:
+        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+        x, lastx = lastx - quotient*x, x
+        y, lasty = lasty - quotient*y, y
+    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
+ 
+def calculate_modular_inverse(a, m):
+	g, x, y = extended_gcd(a, m)
+	if g != 1:
+		raise ValueError
+	return x % m
 
 def crack_unknown_increment(states, modulus, multiplier):
     increment = (states[1] - states[0]*multiplier) % modulus
@@ -39,6 +54,7 @@ def crack_unknown_modulus(states):
     diffs = [s1 - s0 for s0, s1 in zip(states, states[1:])]
     zeroes = [t2*t0 - t1*t1 for t0, t1, t2 in zip(diffs, diffs[1:], diffs[2:])]
     modulus = abs(reduce(gcd, zeroes))
+    print(modulus - 9223372036854775783)
     return crack_unknown_multiplier(states, modulus)
 
 def predict_next_value(current_state, modulus, multiplier, increment):
@@ -46,9 +62,9 @@ def predict_next_value(current_state, modulus, multiplier, increment):
     return next_value
 
 def test():
-    #seed = int(input("Give the seed: "))
-    seed = 32142145
-    number_of_initial_values = 6
+    seed = int(input("Give the seed: "))
+    #seed = 32142145
+    number_of_initial_values = 60
     number_of_values_to_predict = 5
     initial_values = []
     
@@ -56,7 +72,7 @@ def test():
     for i in range(number_of_initial_values):
         initial_values.append(lcg.generate_next_value())
 
-    print("First {} values from the LCG: {}".format(number_of_initial_values, initial_values))
+    #print("First {} values from the LCG: {}".format(number_of_initial_values, initial_values))
 
     algorithm_params = crack_unknown_modulus(initial_values)
     modulus, multiplier, increment = algorithm_params
@@ -77,7 +93,5 @@ def test():
         actual_values.append(lcg.generate_next_value())
 
     print("Actual {} next values: {}".format(number_of_values_to_predict, actual_values))
-
-
 
 test()
